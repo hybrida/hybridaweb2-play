c13c51724f18f9080caad2666205b150c3f049d0
Kevin R. Stravers <kevinrs@stud.ntnu.no>
2014-09-03 00:12:24 +0200

    Removed db test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,23 +11,8 @@ import play.db.*;
 public class Application extends Controller {
 
     public static Result index() throws java.sql.SQLException {
-
-    	javax.sql.DataSource ds = DB.getDataSource();
-    	java.sql.Connection dc = ds.getConnection();
-    	java.sql.Statement st = dc.createStatement();
-  		java.sql.ResultSet re = st.executeQuery("SELECT * FROM users");
-
-  		java.util.ArrayList<String> names = new java.util.ArrayList<String>();
-  		for (int i = 1;; ++i)
-		{
-			boolean x = re.absolute(i);
-			if (!x)
-				break;
-			String str = re.getString("name");
-			names.add(str);
-		}
-
-    	return ok(layout.render("Hybrida", names));
+	java.util.ArrayList<String> names = new java.util.ArrayList<String>();    	
+return ok(layout.render("Hybrida", names));
     }
 
     public static Result getter(String str) {
