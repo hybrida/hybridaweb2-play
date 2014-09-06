103976c97b45b9694c63d27f6fb97b27f04a6f32
EliasBHagen <Elias.B.Hagen@Gmail.com>
2014-09-06 16:56:04 +0200

    Lol page

--- app/controllers/Lol.java
+++ app/controllers/Lol.java
@@ -10,10 +10,14 @@ import java.util.ArrayList;
  * Created by eliasbragstadhagen on 06.09.14.
  */
 public class Lol extends Controller{
-    public static Result index(){
+    public static Result index(String name){
         ArrayList<String> content = new ArrayList<>();
         content.add("<h1> Hybrida proffetional MLG diamond leauge Lol team:<h1> <p> <p>");
-        content.add("Dj Syndeflood");
+        content.add("Dj Syndeflood <p>");
+        if (name != null){
+            content.add(name);
+            content.add("<p>");
+        }
         return ok(layout.render("Hybrida MLG proffesional LoL team: ", content));
     }
 }
