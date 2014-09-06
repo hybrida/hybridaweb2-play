a748bdacaec21baefc922e453b80d5d21e67fc42
EliasBHagen <Elias.B.Hagen@Gmail.com>
2014-09-06 23:52:58 +0200

    Lol page

--- app/controllers/Lol.java
+++ app/controllers/Lol.java
@@ -22,6 +22,15 @@ public class Lol extends Controller{
 
         return ok(layout.render("Hybrida MLG proffesional LoL team: ", content));
     }
+    public class Lol extends Controller{
+        public static Result index(){
+            ArrayList<String> content = new ArrayList<>();
+            content.add("<img src=\"/assets/images/mlg.jpg\" alt=\"rect\">");
+            content.add("<h1> Hybrida proffesional MLG diamond league Lol team:<h1> <p> <p>");
+            content.add("Dj Syndeflood <p>");
+
+            return ok(layout.render("Hybrida MLG proffesional LoL team: ", content));
+        }
 }
 
 
