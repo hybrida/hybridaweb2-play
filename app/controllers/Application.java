7f75f80a807ff331aab9ed8a3ecd3cf39997dac2
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-06 17:29:43 +0200

    Added recursion

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,13 +16,24 @@ public class Application extends Controller {
     }
 
     public static Result getter(String str) {
-    	java.util.ArrayList<String> stuff_to_render = new java.util.ArrayList<String>();
-    	stuff_to_render.add("<h1>This is not escaped</h1>");
-    	stuff_to_render.add("<h2>1337</h2>");   
-    	stuff_to_render.add("<h3>#route</h3>"); 
-    	stuff_to_render.add("<h3>#route #MLG #yoloswag ( ͡° ͜ʖ ͡°)</h3>");
-    	stuff_to_render.add("<img src=\"/assets/images/5f0.png\" alt=\"sweaty face\">");
-    	return ok(layout.render("A message was recorded: " + str, stuff_to_render));
+    	java.util.ArrayList<String> is = new java.util.ArrayList<String>();
+    	java.util.ArrayList<String> is2 = new java.util.ArrayList<String>();
+    	
+    	for (int i = 0; i < 5; ++i) {
+	    	is.add(new Integer(i).toString());
+	    	String x = layout.render("I dunno m89", is).toString();
+	    	is.remove(is.size() - 1);
+	    	is.add(x);
+	    }
+
+    	is.add("<h1>This is not escaped</h1>");
+    	is.add("<h2>1337</h2>");   
+    	is.add("<h3>#route</h3>"); 
+    	is.add("<h3>#route #MLG #yoloswag ( ͡° ͜ʖ ͡°)</h3>");
+    	is.add("<img src=\"/assets/images/5f0.png\" alt=\"sweaty face\">");
+
+
+    	return ok(layout.render("A message was recorded: " + str, is));
     }
 
 
