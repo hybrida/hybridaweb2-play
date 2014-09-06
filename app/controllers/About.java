025f7636258547cbf985205ef7d77d1ca11d4ff2
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-06 16:00:46 +0200

    Added lower case

--- app/controllers/About.java
+++ app/controllers/About.java
@@ -19,7 +19,7 @@ public class About extends Controller {
 
     public static Result personInfo(String name) {
         java.util.ArrayList<String> names = new java.util.ArrayList<String>();      
-        names.add(describe(name));
+        names.add(describe(name.toLowerCase()));
         return ok(layout.render("Hybrida", names));
     }
 
