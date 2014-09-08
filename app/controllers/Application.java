61c45e37ffd4e2472b6eaaf776d73bf80a8262e2
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-08 17:47:21 +0200

    Fixed layout rendering in Application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,19 @@ import play.*;
 import play.mvc.*;
 import views.html.*;
 
+import models.*;
 
 import play.db.*;
 
 
 public class Application extends Controller {
+
+	final static play.data.Form<Search> userForm = play.data.Form.form(Search.class);
+
+	public static play.twirl.api.Html renderMainLayout(String title, java.util.ArrayList<String> content) {
+    	play.data.Form<Search> userForm = play.data.Form.form(Search.class);
+    	return layout.render(title, content);
+	}
 
     public static Result index() throws java.sql.SQLException {
     	java.util.ArrayList<String> names = new java.util.ArrayList<String>();    	
@@ -24,13 +32,25 @@ public class Application extends Controller {
 			if (Integer.parseInt(x) > 10)
 				names.add("You're a true hybrid");
     	}
-		return ok(layout.render("Hybrida", names));
+
+    	userForm.fill(new Search());
+
+    	return ok(Application.renderMainLayout("Hybrida", names));
+    }
+
+    public static Result processForm() {
+    	play.data.Form<Search> s = userForm.bindFromRequest();
+    	Search x = s.get();
+    	if (x.term != null)
+    		return ok(x.term);
+    	return badRequest("Whut");
     }
 
     public static Result show404(String get_value) {
     	java.util.ArrayList<String> four = new java.util.ArrayList<String>();
     	four.add(notFoundErrorPage.render(get_value).toString());
-    	return notFound(layout.render("404", four));
+    	//play.data.Form<Search> userForm = play.data.Form.form(Search.class);
+    	return notFound(renderMainLayout("404", four));
     }
 
 
