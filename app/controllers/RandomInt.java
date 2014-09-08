4df75313de471b81c98058113490ea7a4e756a27
simennj <simennj@outlook.com>
2014-09-08 09:43:10 +0200

    la til en side med en tilfeldig tallgenerator /random

---
+++ app/controllers/RandomInt.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import models.RandomForm;
+import play.data.Form;
+import play.mvc.*;
+import views.html.randomview;
+
+import java.util.Random;
+
+import static play.data.Form.form;
+
+
+public class RandomInt extends Controller{
+
+    final static Form<RandomForm> randomForm = form(RandomForm.class);
+
+    public static Result randomPage(){
+        RandomForm created = new RandomForm();
+        created.numberTo = 1;
+        return ok(randomview.render(randomForm,"",created));
+    }
+
+    public static Result newRandom(){
+        Random rand = new Random();
+        Form<RandomForm> input = randomForm.bindFromRequest();
+
+        if(input.hasErrors()){
+            RandomForm created = new RandomForm();
+            created.numberTo = 1;
+            return ok(randomview.render(randomForm,"",created));
+        }else {
+            RandomForm saved = input.get();
+            int numberFrom = input.get().numberFrom;
+            int numberTo = input.get().numberTo;
+            if (numberFrom < numberTo) {
+                int generated = rand.nextInt((numberTo - numberFrom) + 1) + numberFrom;
+                return ok(randomview.render(randomForm, generated + " er tilfeldig nok", saved));
+            } else {
+                return ok(randomview.render(randomForm, numberTo + " er ikke større enn " + numberFrom, saved));
+            }
+        }
+    }
+
+
+}
