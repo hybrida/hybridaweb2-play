cbf09179f260e5ee73065a59feacbd4defa76da7
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-09 20:02:59 +0200

    ExampleDatabase update for iteration

--- app/controllers/ExampleDatabase.java
+++ app/controllers/ExampleDatabase.java
@@ -17,9 +17,21 @@ public class ExampleDatabase extends Controller {
                 "INSERT INTO company VALUES (1, 'Something');" +
                 "INSERT INTO company VALUES (2, 'Nothing');");
         */
-        ResultSet result = statement.executeQuery("SELECT * FROM company");
+
+        ResultSet result = statement.executeQuery("SELECT COUNT(*) FROM company");
         result.absolute(1);
-        return ok(layoutHtml.render("Hybrida", play.twirl.api.Html.apply("<h1>" + result.getString(2) + "</h1>")));
+        int p = result.getInt(1);
+        result = statement.executeQuery("SELECT * FROM company");
+        String x = "";
+        for (int i = 1; i <= p; ++i) {
+            result.absolute(i);
+            x += result.getString(2) + "<br>";
+        }
+        return ok(layoutHtml.render("Hybrida", play.twirl.api.Html.apply(x)));
+        //ResultSet result = statement.executeQuery("SELECT * FROM company");
+
+        //result.absolute(1);
+        //return ok(layoutHtml.render("Hybrida", play.twirl.api.Html.apply("<h1>" + result.getString(2) + "</h1>")));
     }
 
 }
