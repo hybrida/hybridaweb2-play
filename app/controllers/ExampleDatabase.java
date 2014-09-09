2029e403b292e3380044b20ced75b3c47888c3bb
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-09 17:35:49 +0200

    Makes the database work

--- app/controllers/ExampleDatabase.java
+++ app/controllers/ExampleDatabase.java
@@ -11,6 +11,11 @@ public class ExampleDatabase extends Controller {
         javax.sql.DataSource ds = DB.getDataSource();
         java.sql.Connection connection = ds.getConnection("sa", "");
         java.sql.Statement statement = connection.createStatement();
+        /*statement.executeUpdate(
+                "INSERT INTO company VALUES (0, 'Hybrida');" +
+                "INSERT INTO company VALUES (1, 'Something');" +
+                "INSERT INTO company VALUES (2, 'Nothing');");
+        */
         ResultSet result = statement.executeQuery("SELECT * FROM company");
         result.absolute(1);
         return ok(layoutHtml.render("Hybrida", play.twirl.api.Html.apply("<h1>" + result.getString(2) + "</h1>")));
