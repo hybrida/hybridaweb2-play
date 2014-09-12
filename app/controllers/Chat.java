6c313c6974be53333eb59b991ccdfe8ff1a9bed1
Thormod Myrvang <thormodm@stud.ntnu.no>
2014-09-12 03:37:30 +0200

    Added simple chat via websockets

---
+++ app/controllers/Chat.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.ChatActor;
+import play.mvc.Controller;
+import akka.actor.*;
+import play.mvc.Result;
+import play.libs.F.*;
+import play.mvc.WebSocket;
+import views.html.chatClient;
+import views.html.layoutHtml;
+
+/**
+ * Created by Tormod on 09.09.2014.
+ */
+public class Chat extends Controller {
+
+    public static Result connectClient(String name) {
+        return ok(layoutHtml.render(name, chatClient.render()));
+    }
+
+    public static WebSocket<String> socket() {
+        return WebSocket.withActor(new Function<ActorRef, Props>() {
+            public Props apply(ActorRef out) throws Throwable {
+                return ChatActor.props(out);
+            }
+        });
+    }
+}
---
+++ app/models/ChatActor.java
@@ -0,0 +1,46 @@
+package models;
+
+import akka.actor.*;
+import play.libs.Akka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tormod on 12.09.2014.
+ */
+public class ChatActor extends UntypedActor {
+    private static List<ChatActor> clients = new ArrayList<ChatActor>();
+    //private final String name;
+
+    public static Props props(ActorRef out) {
+        return Props.create(ChatActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public ChatActor(ActorRef out) {
+        this.out = out;
+        clients.add(this);
+        //this.name = "Anonymous";
+        tellAll("&A chatter connected");
+    }
+
+    private void tellAll(String message) {
+        for (ChatActor client : clients) {
+            if (!client.equals(this)) client.out.tell(message, self());
+        }
+    }
+
+    public void postStop() throws Exception {
+        clients.remove(this);
+        tellAll("&A chatter disconnected");
+    }
+
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            message = ((String) message).charAt(0)=='&' ? ((String) message).substring(1) : ((String) message);
+            tellAll((String) message);
+        }
+    }
+}
