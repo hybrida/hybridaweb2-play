b8d4d55597e7c50ec68cac1f78bd852f59a2cb5b
Ivar H. Kråbøl <ivarhk@stud.ntnu.no>
2014-09-16 19:25:39 +0200

    Added Article-model

---
+++ app/models/Article.java
@@ -0,0 +1,33 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by Ivar on 16.09.2014.
+ */
+@Entity
+public class Article extends Model {
+    private String title;
+    private String content;
+    private User author;
+    private Timestamp posted;
+
+    public Article(String title, String content, User author, Timestamp posted) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.posted = posted;
+    }
+
+    public String getTitle() { return title; }
+
+    public String getContent() { return content; }
+
+    public String getAuthorName() { return author.getName(); }
+
+    public Date getPosted() { return new Date(posted.getTime()); }
+}
