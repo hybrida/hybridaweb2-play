36d966e42d876176b818949dbc94c4225b5f0b83
bourgondaries <kevinrs@stud.ntnu.no>
2014-09-16 17:21:28 +0200

    Apply SSO edits

--- app/controllers/ExampleSSO.java
+++ app/controllers/ExampleSSO.java
@@ -6,7 +6,10 @@ import play.mvc.Result;
 
 public class ExampleSSO extends Controller {
 
+    public static String innsida_login_link = "https://innsida.ntnu.no/sso/?target=hybridaweb&returnargs=";
+
     public static Result index() {
+        String toroute = routes.Application.index().absoluteURL(request());
         return redirect("https://innsida.ntnu.no/sso/?target=hybridaweb&returnargs=");
     }
 }
