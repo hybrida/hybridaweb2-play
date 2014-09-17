8a3633b0320ed3c63f51195ab280ebe279196c95
Øystein Hauan <oysteibh@stud.ntnu.no>
2014-09-17 16:28:02 +0200

    Random jazz

--- app/controllers/Oystein.java
+++ app/controllers/Oystein.java
@@ -16,8 +16,12 @@ public class Oystein extends Controller{
     public static Result someCode(String number){
         int num = Integer.parseInt(number);
         ArrayList<String> list = new ArrayList<String>();
+        if (num > 200){
+            num = 200;
+            list.add("<p><h1>Ingen på Hybrida tar så mange pushups, din lyvende jævel!</h1></p>");
+        }
         for (int i = 0; i<num; i++){
-            list.add("<p><h1>Antall pushups hittil: " + i + "</h1></p>");
+            list.add("<p><h1>Antall pushups hittil: " + (i + 1) + "</h1></p>");
         }
 
         return ok(layout.render("Jassejass", list));
