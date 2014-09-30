3c23d0f894fc0584de1fa047aad72f35f1c4995f
bourgondaries <kevinrs@stud.ntnu.no>
2014-10-01 01:29:00 +0200

    Clean up ExampleSSO and create a model to handle the dirty internals of it

--- app/controllers/ExampleSSO.java
+++ app/controllers/ExampleSSO.java
@@ -1,15 +1,8 @@
 package controllers;
 
-import models.HttpRequestData;
 import play.mvc.Controller;
-import scala.util.matching.Regex$;
 import views.html.*;
 import play.mvc.Result;
-
-import java.io.FileInputStream;
-import java.nio.file.Paths;
-import java.security.*;
-import java.util.regex.Pattern;
 
 public class ExampleSSO extends Controller {
 
@@ -19,55 +12,42 @@ public class ExampleSSO extends Controller {
         return ok("");
     }
 
-    // TODO: Put most code into a model class
-    public static Result verifylogin() throws java.io.IOException, java.security.cert.CertificateException, NoSuchAlgorithmException, SignatureException, InvalidKeyException, NoSuchProviderException {
-        HttpRequestData http_data = new HttpRequestData();
-        String data = http_data.get("data");
-        String sign64 = http_data.get("sign");
-        String clientip = http_data.get("clientip");
+    public static Result verifylogin() {
+        models.SSOData data = new models.SSOData();
 
-        ;
-        byte[] sign = java.util.Base64.getDecoder().decode(sign64);
 
-        // Create a hashmap of all the data.
-        String[] data_separated = data.split(",");
-        java.util.Map<String, String> values = new java.util.HashMap<String, String>();
-        for (int i = 0; i < data_separated.length; ++i) {
-            if (i + 1 < data_separated.length)
-                values.put(data_separated[i + 0], data_separated[i + 1]);
-        }
-        String username = values.get("username");
-        String encrypted_username = play.api.libs.Crypto.encryptAES(username);
-
-        java.io.FileInputStream filestream = new java.io.FileInputStream(new java.io.File("innsida.crt"));
-        java.security.cert.CertificateFactory cf = java.security.cert.CertificateFactory.getInstance("X.509");
-        java.security.cert.Certificate cert = cf.generateCertificate(filestream);
-
-        java.security.PublicKey pubkey = cert.getPublicKey();
         try {
-            if (verifySignature(data, pubkey, sign)) {
-                System.out.println("It works YEAH!");
-            } else {
-                System.out.println("It didnt sign!!");
+            String return_url = new models.HttpRequestData().get("returnargs");
+            if (data.login()) {
+
+                /// TODO HERE:
+                /**
+                 * 1. We know now that the data is sound and valid. The next thing to do is to check if the
+                 * given username is in the database, and that it is possible to set it to "logged in".
+                 * The reason for this extra security is to avoid having sneaky packet duplicators
+                 * logging in to multiple accounts simultaneously.
+                 * PROBLEM: If a user disconnects/clears cookies he will not be able to login again,...
+                 *
+                 * 2. Generate a salty cookie encrypted with at least 256 bit AES containing the username.
+                 * The cookie must use the "time" given in the SSO request header. This allows each login to give a uniquely
+                 * salted and encrypted username. Thus; no one can ever duplicate a header or cookie.
+                 *
+                 * 3. Generate correct code for each page's login button as to redirect you to that specific page.
+                 * In addition; we must put code here reading from "returnargs".
+                 *
+                 * 4. Put code in the else clause notifying the user has failed to log in correctly.
+                 */
+
+                System.out.println(data.getLoginInfo().get("username"));
+                return redirect(return_url);
             }
-        } catch (InvalidKeyException inv) {
-            return ok(escapeText.render(inv.toString()));
-        } catch (NoSuchProviderException noprov) {
-            return ok(escapeText.render(noprov.getMessage() + "<br>" + sign));
-        } catch (java.lang.Exception e) {
-            return ok(escapeText.render(e.toString()));
+            else {
+                session().clear();
+                return redirect(routes.Application.index().url());
+            }
+        } catch (Exception exc_obj) {
+            return ok(escapeText.render(exc_obj.toString()));
         }
-
-        session("LOGGED IN COMPLETED", encrypted_username);
-
-        return redirect(routes.Application.index().url());
-    }
-
-    public static boolean verifySignature(String data, PublicKey key, byte[] signature) throws Exception {
-        Signature signer = Signature.getInstance("SHA1withRSA");
-        signer.initVerify(key);
-        signer.update(data.getBytes());
-        return (signer.verify(signature));
     }
 
     public static Result index() {
