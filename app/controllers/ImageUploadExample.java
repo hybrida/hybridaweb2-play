f80172cd7c6edc662b995df35838ba742be75442
EliasBHagen <Elias.B.Hagen@Gmail.com>
2014-09-30 20:25:06 +0200

    Changed background in main.css.content temporary changes in ImageUploadExample

--- app/controllers/ImageUploadExample.java
+++ app/controllers/ImageUploadExample.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import play.api.mvc.Call;
 import play.mvc.Http;
 import views.html.ImUpView;
 import play.mvc.Result;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
+import static controllers.Lol.toHtml;
 import static play.mvc.Controller.flash;
 import static play.mvc.Controller.request;
 import static play.mvc.Results.ok;
@@ -20,14 +26,18 @@ public class ImageUploadExample {
        return ok(render("Image", ImUpView.render()));
     }
 
-    public static Result upload() {
+    static final Call savePlace = routes.Assets.at("Upload");
+
+    public static Result upload() throws FileNotFoundException {
         Http.MultipartFormData body = request().body().asMultipartFormData();
         Http.MultipartFormData.FilePart picture = body.getFile("picture");
         if (picture != null) {
             String fileName = picture.getFilename();
             String contentType = picture.getContentType();
             File file = picture.getFile();
-            return ok("Upload Successful");
+            boolean her = file.renameTo(new File(savePlace + fileName));
+
+            return ok("Upload Successful"+"<img src=\"/assets/Upload/"+fileName+"\" alt=\"rect\"/>"+her);
         } else {
             flash("error", "Missing file");
             return ok("file missing");
