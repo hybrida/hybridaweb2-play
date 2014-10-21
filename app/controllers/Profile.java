3f7f1e1369af7f1dc9adb16bb5a405155d924502
Ivar H. Kråbøl <ivarhk@stud.ntnu.no>
2014-10-21 20:00:37 +0200

    Adds profile view (for later implementation with controller and Uesr model)

---
+++ app/controllers/Profile.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by Ivar on 21.10.2014.
+ */
+public class Profile extends Controller {
+    public static Result index(String username) {
+        return TODO;
+    }
+}
