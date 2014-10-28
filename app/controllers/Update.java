fe322dfba9a4062f8eec89063ca455d7764942db
sindrefl <sindre.flood@gmail.com>
2014-10-28 19:52:25 +0100

    legger inn updatebildet, ny plassering legger til mulighet til å laste opp nye update TODO: begrense til admin

--- app/controllers/Update.java
+++ app/controllers/Update.java
@@ -1,9 +1,18 @@
 package controllers;
 
+import org.apache.commons.io.FileUtils;
+import play.mvc.Http;
 import play.mvc.Result;
 import views.html.layoutHtml;
 import views.html.update;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static controllers.Lol.toHtml;
+import static play.mvc.Controller.flash;
+import static play.mvc.Controller.request;
 import static play.mvc.Results.TODO;
 import static play.mvc.Results.ok;
 
@@ -16,6 +25,32 @@ public class Update {
     public static Result index(){
 
         return ok(layoutHtml.render("Update!", update.render()));
+    }
+
+
+    public static Result post() throws FileNotFoundException {
+        Http.MultipartFormData body = request().body().asMultipartFormData();
+        Http.MultipartFormData.FilePart picture = body.getFile("picture");
+        if (picture != null && picture.getContentType().contains("pdf")) {
+            String fileName = picture.getFilename();
+            String contentType = picture.getContentType();
+            File file = picture.getFile();
+            try{
+                FileUtils.moveFile(file, new File("public/update", fileName));
+            } catch (IOException ioe) {
+                System.out.println("Problem operating on filesystem");
+            }
+
+            return ok(layoutHtml.render("img",toHtml("Upload Successful" + "<img src=\"/assets/update/" + fileName + "\" alt=\"rect\"/>")));
+        }else if(picture != null){
+            flash("error", "Invalid file");
+            return ok("Invalid file, accepted format: pdf");
+        }
+
+        else {
+            flash("error", "Missing file");
+            return ok("file missing");
+        }
     }
 
     public static Result utgaver(){
