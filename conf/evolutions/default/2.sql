b79784aa96370c1c0696032d4aabd42e32eb04c8
bourgondaries <kevinrs@stud.ntnu.no>
2014-11-04 18:48:30 +0100

    Implement consistent admin namge usage. it must be \hybrid\ without slashes.

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-INSERT INTO USER (username, student, bedkom, admin, root) VALUES ('root', true, true, true, true);
+INSERT INTO USER (username, student, bedkom, admin, root) VALUES ('hybrid', true, true, true, true);
 INSERT INTO USER (username, first_name, surname) VALUES ('kevinrs', 'Kevin Robert', 'Stravers');
 INSERT INTO USER (username, first_name, surname) VALUES ('thormodm', 'Thormod', 'Myrvang');
 # --- !Downs
