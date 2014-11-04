b24c6a27a415716dd8f6ea0c216fb4bc90da2b57
bourgondaries <kevinrs@stud.ntnu.no>
2014-11-04 21:51:04 +0100

    Clean up user state and login state

--- app/models/User.java
+++ app/models/User.java
@@ -42,6 +42,8 @@ public class User extends Model {
 
     public User() {}
 
+
+
     public User(String first_name, String surname) {
         this.first_name = first_name;
         this.surname = surname;
