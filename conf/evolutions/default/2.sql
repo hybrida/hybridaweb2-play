3e772d143954954c6d911e815d145c83b2811995
bourgondaries <kevinrs@stud.ntnu.no>
2014-11-04 19:03:30 +0100

    Fix to single comment quotes

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-INSERT INTO USER (username, first_name, surname, student, bedkom, admin, root) VALUES ('hybrid', "r00t", "xXxS00p3rUz3rXXx", true, true, true, true);
+INSERT INTO USER (username, first_name, surname, student, bedkom, admin, root) VALUES ('hybrid', 'r00t', 'xXxS00p3rUz3rXXx', true, true, true, true);
 INSERT INTO USER (username, first_name, surname) VALUES ('kevinrs', 'Kevin Robert', 'Stravers');
 INSERT INTO USER (username, first_name, surname) VALUES ('thormodm', 'Thormod', 'Myrvang');
 # --- !Downs
