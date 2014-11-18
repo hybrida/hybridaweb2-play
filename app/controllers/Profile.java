d0e0f28645b6c12478e56ffdcb617cd061fc8b27
bourgondaries <kevinrs@stud.ntnu.no>
2014-11-18 18:53:06 +0100

    Add edit function in Profile

--- app/controllers/Profile.java
+++ app/controllers/Profile.java
@@ -24,6 +24,37 @@ public class Profile extends Controller {
         return ok(layoutHtml.render(user.getName(), profile.render(user)));
     }
 
+    public static Result editUserData() {
+        models.HttpRequestData data = new models.HttpRequestData();
+        if (LoginState.getUser().username == data.get("username")) {
+            User user = LoginState.getUser();
+            
+            user.first_name = data.get("first_name");
+            user.surname = data.get("surname");
+            user.middle_name = data.get("middle_name");
+            user.email = data.get("email");
+            user.website_url = data.get("website_url");
+            user.phone = data.get("phone");
+            user.title = data.get("title"); // Ph.D., Civ.Eng., Stud., Chief, Commander, General, Lord, Admiral, Vevsjef,...
+            user.profile_image_file_name = data.get("profile_image_file_name");
+            //  int         graduation_year = 0;
+
+            // Privilege status
+            /*user.student = data.get("student") == "true" ? true : false;    // No special privileges.
+            user.bedkom = data.get("bedkom") == "true" ? true : false;     // Control over bedpress.
+            
+            user.admin = data.get("admin") == "true" ? true : false;      // For control over the entire page. Check your privilege
+            user.root = data.get("root") == "true" ? true : false;       // Powers too great for mere mortals.
+            user.sex = data.get("sex") == "true" ? true : false;         // For specific events.
+
+            user.enrolled = data.get("enrolled");    // For specific bedpresses requiring a year number.
+            user.date_of_birth = data.get("date_of_birth");
+            */
+            user.save();
+        }
+        return ok();
+    }
+
     public static boolean authorized(String username) {
         User loggedInUser = LoginState.getUser();
         return username == loggedInUser.getUsername() || loggedInUser.admin || !loggedInUser.root;
