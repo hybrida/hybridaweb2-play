70907fd6bc157f0064fa8e103d92256f81f35d28
torstengit config --global user.email tbm94@live.nogit config --global user.email tbm94git config --global user.name torsten <tbm94@live.no>
2015-02-24 20:44:57 +0100

    do nice to page griffens orden

--- app/trash/controllers/griffensOrden.java
+++ app/trash/controllers/griffensOrden.java
@@ -10,6 +10,6 @@ import views.html.NyStudent.*;
  * Created by Torsten on 11.02.15
  */
 public class griffensOrden extends Controller {
-    final static public Result index(){  return ok(layoutWithHead.render("",views.html.griffensOrdenhead.render(), views.html.griffensOrdenbody.render()));
+    final static public Result index(){  return ok(layoutWithHead.render("Griffens Orden", views.html.griffensOrdenbody.render(),views.html.griffensOrdenhead.render()));
     }
 }
