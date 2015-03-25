8f6447167e2738aed8dc65eb8dca0b94b1ab6fb0
Thormod Myrvang <thormodm@stud.ntnu.no>
2015-03-25 13:20:10 +0100

    Make calendar display events

--- app/controllers/Calendar.java
+++ app/controllers/Calendar.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import com.avaje.ebean.Expr;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import models.*;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -15,29 +18,49 @@ import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Calendar extends Controller {
+    private final static String ISO8601 = "yyyy-MM-dd";
+
     public static Result index() {
         return ok(layoutWithHead.render("Kalender", views.html.Calendar.calendarHead.render(), views.html.Calendar.calendarBody.render()));
     }
 
-    public static Result fetch(String start, String end, String zone) {
-        Date start_date;
+    public static Result fetch(String start, String end) {
+        Date start_date; //title, start, end, url
         Date end_date;
 
         try {
-            start_date = new SimpleDateFormat("yyyy-MM-dd").parse(start);
-            end_date = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+            start_date = new SimpleDateFormat(ISO8601).parse(start);
+            end_date = new SimpleDateFormat(ISO8601).parse(end);
         } catch (ParseException e) {
             return badRequest();
         }
 
-        JsonNode json = Json.toJson(models.Event.find.where().between("eventHappens", new java.sql.Date(start_date.getTime()), new java.sql.Date(end_date.getTime()))); //timeFrame - duration
-        return ok(json);
+        List<models.Event> events = models.Event.find.where().or(
+                Expr.between("eventHappens", new Timestamp(start_date.getTime()), new Timestamp(end_date.getTime())),
+                Expr.between("eventHappens", new Timestamp(start_date.getTime()), new Timestamp(end_date.getTime())) //TODO: Read end date
+        ).findList();
+
+        List<ObjectNode> reformatted_list_json = new ArrayList<>();
+
+        for (models.Event event : events) {
+            JsonNode json = Json.toJson(event);
+            ObjectNode reformatted = Json.newObject();
+            reformatted.set("title", new TextNode(json.findValue("articleId").asText())); //TODO: Find title
+            reformatted.set("start", new TextNode(new SimpleDateFormat(ISO8601).format(json.findValue("eventHappens").asLong())));
+            //reformatted.set("end", new TextNode(json.get("articleId").asText())); //TODO: Set end
+            reformatted.set("url", new TextNode("artikkel/"+json.findValue("articleId").asText()));
+            reformatted_list_json.add(reformatted);
+        }
+
+        return ok(reformatted_list_json.toString());
     }
 
     public static Result calendarRender() {
