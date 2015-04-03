513a6ad9157b6eeee87b147532e44cb3743b80a5
Teodor Heggelund <teodor.heggelund@gmail.com>
2015-04-03 02:03:03 +0200

    Early demo code using AngularJS. Part 2.

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -2,14 +2,23 @@
 	var app = angular.module('store', []);
 
     app.controller('StoreController', function () {
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: '. . .',
-        canPurchase: true,
-        soldOut: false
-    };
+    var gems =
+        [
+            {
+                name: 'Dodecahedron',
+                price: 2,
+                description: '. . .',
+                canPurchase: true,
+                soldOut: false
+            },
+            {
+                name: 'Pentagonal Gem',
+                price: 5.95,
+                canPurchase: true,
+                soldOut: true
+            }
+        ]
 })();
