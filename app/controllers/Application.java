72406fa645a57f52a8832895bf772cf4d3eb99cc
BourgondAries <macocio@gmail.com>
2015-09-10 19:37:40 +0200

    Normalize Application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,80 +10,79 @@ import static models.Article.getArticleData;
 
 public class Application extends Controller {
 
-    /**
-     * \brief Application class, handles basic site functionality.
-     *
-     * Handles basic functionality and responses. Handles index site,
-     * unauthorized access requests, 404 and 400 results, etc.
-     *
-     */
+	/**
+	* \brief Application class, handles basic site functionality.
+	*
+	* Handles basic functionality and responses. Handles index site,
+	* unauthorized access requests, 404 and 400 results, etc.
+	*
+	*/
 
-    /**
-     * \brief Index entry point of the website.
-     */
-    public static Result index() {
-        java.util.List<Renderable> articles = models.Renders.getVisibleRenderables();
-        String concatenation = "";
-        int i = 0;
-        for (Renderable renderable : Lists.reverse(articles)) {
-            ++i;
-            if (i <= 2)
-                concatenation += "<div class='col-md-6' style='padding: 1%'>";
-            else
-                concatenation += "<div class='col-md-3'style='padding: 1%'>";
-            concatenation += renderable.renderFrontPageSample();
-            concatenation += "</div>";
-        }
-        return ok(layout.render("Hybrida", views.html.Application.index.render(views.html.utils.toHtml.render(concatenation))));
-    }
+	/**
+	* \brief Index entry point of the website.
+	*/
+	public static Result index() {
+		java.util.List<Renderable> articles = models.Renders.getVisibleRenderables();
+		String concatenation = "";
+		int i = 0;
+		for (Renderable renderable : Lists.reverse(articles)) {
+			++i;
+			if (i <= 2)
+				concatenation += "<div class='col-md-6' style='padding: 1%'>";
+			else
+				concatenation += "<div class='col-md-3'style='padding: 1%'>";
+			concatenation += renderable.renderFrontPageSample();
+			concatenation += "</div>";
+		}
+		return ok(layout.render("Hybrida", views.html.Application.index.render(views.html.utils.toHtml.render(concatenation))));
+	}
 
-    /**
-     * Useful functions to use to return standard messages to the client.
-     * @return
-     */
-    public static Result showUnauthorizedAccess() {
-        return unauthorized(layout.render("Unauthorized", views.html.Application.showUnauthorizedAccess.render()));
-    }
+	/**
+	* Useful functions to use to return standard messages to the client.
+	* @return
+	*/
+	public static Result showUnauthorizedAccess() {
+		return unauthorized(layout.render("Unauthorized", views.html.Application.showUnauthorizedAccess.render()));
+	}
 
-    public static Result show404(String get_value) {
-    	return notFound(layout.render("404", views.html.Application.show404.render(get_value)));
-    }
+	public static Result show404(String get_value) {
+		return notFound(layout.render("404", views.html.Application.show404.render(get_value)));
+	}
 
-    public static Result show400(String get_value) {
-        return badRequest(layout.render("400", views.html.Application.show400.render(get_value)));
-    }
+	public static Result show400(String get_value) {
+		return badRequest(layout.render("400", views.html.Application.show400.render(get_value)));
+	}
 
-    /**
-      \brief Check if the user has the ability to edit; if not: it returns a non-null Result.
-    */
-    public static Result checkEditPrivilege(User user) {
-        if (user.isDefault()) {
-          return controllers.Application.show400("Du må logge inn for å endre arransjement.");
-        } if (!user.canCreateNewArticle()) {
-          return controllers.Application.show400("Du har ikke rettighetene til å endre på arransjementer.");
-        }
-        return null;
-    }
+	/**
+		\brief Check if the user has the ability to edit; if not: it returns a non-null Result.
+	*/
+	public static Result checkEditPrivilege(User user) {
+		if (user.isDefault()) {
+			return controllers.Application.show400("Du må logge inn for å endre arransjement.");
+		} if (!user.canCreateNewArticle()) {
+			return controllers.Application.show400("Du har ikke rettighetene til å endre på arransjementer.");
+		}
+		return null;
+	}
 
-    /**
-     * Experimental code
-     * @return
-     * @throws java.sql.SQLException
-     */
-    public static Result proto()  throws java.sql.SQLException {
-        java.util.List<Renderable> articles = models.Renders.getVisibleRenderables();
-        String concatenation = "";
-        for (Renderable renderable : articles) {
-            concatenation += renderable.renderFrontPageSample();
-        }
-        if (true)
-            return ok(views.html.protoFrontPage.render(play.twirl.api.Html.apply(concatenation)));
-        return ok(views.html.protoFrontPage.render(play.twirl.api.Html.apply(getArticleData())));
+	/**
+	* Experimental code
+	* @return
+	* @throws java.sql.SQLException
+	*/
+	public static Result proto()  throws java.sql.SQLException {
+		java.util.List<Renderable> articles = models.Renders.getVisibleRenderables();
+		String concatenation = "";
+		for (Renderable renderable : articles) {
+			concatenation += renderable.renderFrontPageSample();
+		}
+		if (true)
+			return ok(views.html.protoFrontPage.render(play.twirl.api.Html.apply(concatenation)));
+		return ok(views.html.protoFrontPage.render(play.twirl.api.Html.apply(getArticleData())));
+		//return ok(views.html.Application.index.render(play.twirl.api.Html.apply("This is some stuff")));
+	}
 
-        //return ok(views.html.Application.index.render(play.twirl.api.Html.apply("This is some stuff")));
-    }
-
-    public static Result bootstrapTest() {
-        return ok(views.html.BootTest.render());
-    }
+	public static Result bootstrapTest() {
+		return ok(views.html.BootTest.render());
+	}
 }
