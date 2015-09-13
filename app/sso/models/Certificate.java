76c6782ce0a0634a4e0433747cad37dd006dc20c
BourgondAries <macocio@gmail.com>
2015-09-13 22:54:11 +0200

    Make the certificate file use tabs instead of spaces, also sort method names

--- app/sso/models/Certificate.java
+++ app/sso/models/Certificate.java
@@ -4,9 +4,9 @@ package models;
  * Holds the link to the certificate file and other information.
  */
 public class Certificate {
-    public static String getPath() {
-        return "innsida.crt";
-    }
-    public static String getSignMethod() { return "X.509"; }
-    public static String getEncryptionMethod() { return "SHA1withRSA"; }
+	public static String getPath() {
+		return "innsida.crt";
+	}
+	public static String getSignMethod() { return "X.509"; }
+	public static String getEncryptionMethod() { return "SHA1withRSA"; }
 }
