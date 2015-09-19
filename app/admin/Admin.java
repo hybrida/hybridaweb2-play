e69dfb50c633cec6ddd567bf08accbf25b9d944c
BourgondAries <macocio@gmail.com>
2015-09-19 22:50:59 +0200

    Add admin login functionality

---
+++ app/admin/Admin.java
@@ -0,0 +1,27 @@
+package admin;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.layout;
+import models.HttpRequestData;
+import models.PasswordHash;
+
+public class Admin extends Controller {
+	public static Result index() {
+		return ok(layout.render("Admin", admin.views.html.loginform.render()));
+	}
+
+	public static Result login() {
+		try {
+			HttpRequestData data = new HttpRequestData();
+			String password = data.get("adminpass");
+			String hash = "1000:273fad43db7f567593c84d7eab74256fb569d9328e21f938:02a4f3199ffc2c12389ca9bfb55a07f4692f180cc13083e0";
+			boolean correct = PasswordHash.validatePassword(password, hash);
+			return ok(correct ? "YES!" : "NO!");
+		} catch (java.security.NoSuchAlgorithmException exc) {
+			return ok(exc.toString());
+		} catch (java.security.spec.InvalidKeySpecException exc) {
+			return ok(exc.toString());
+		}
+	}
+}
