aebc8389cd1805a55a1ad088d9a6ff72f4c8bdc3
Ivar H. Krabol <ivarhk@gmail.com>
2015-09-22 20:47:21 +0200

    Removes unused unfinished function from main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,3 @@
-OpenOptions = function() {
-    x = document.getElementById("event");
-    if(document.getElementById("isEvent").checked == true) {
-    x.innerHTML = '<input type="text" value="Antall" name="Mengde">'
-    }
-    else{x.innerHTML = ""}
-}
 clearSelf = function(obj) {
     obj.innerHTML="";
 }
