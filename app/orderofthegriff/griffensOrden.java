c9cf05bc52843fe00286bb8f65d6e47a4ae24bfd
torstengit config --global user.email tbm94@live.nogit config --global user.email tbm94git config --global user.name torsten <tbm94@live.no>
2015-09-22 20:09:13 +0200

    fix controller for griffensorden

--- app/orderofthegriff/griffensOrden.java
+++ app/orderofthegriff/griffensOrden.java
@@ -9,6 +9,6 @@ import views.html.layoutWithHead;
  * Created by Torsten on 11.02.15
  */
 public class griffensOrden extends Controller {
-    final static public Result index() {  return ok(layoutWithHead.render("Griffens Orden", views.html.griffensOrdenbody.render(),views.html.griffensOrdenhead.render()));
+    final static public Result index() {  return ok(layoutWithHead.render("Griffens Orden", orderofthegriff.views.html.griffensOrdenbody.render(),orderofthegriff.views.html.griffensOrdenhead.render()));
     }
 }
