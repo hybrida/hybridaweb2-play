bdbb188a4ff19a7c83de6bf2bf119938fbf37c2a
torstengit config --global user.email tbm94@live.nogit config --global user.email tbm94git config --global user.name torsten <tbm94@live.no>
2015-10-06 20:15:22 +0200

    add some good shit

---
+++ app/models/Nominasjon.java
@@ -0,0 +1,12 @@
+package models;
+
+import com.avaje.ebean.annotation.CreatedTimestamp;
+import play.db.ebean.Model;
+import play.twirl.api.Html;
+
+import javax.persistence.*;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
