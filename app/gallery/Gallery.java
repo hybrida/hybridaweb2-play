c0e3b6a032e98f65fb629f8626076707b0d70c28
Thormod Myrvang <thormodm@stud.ntnu.no>
2015-10-20 20:47:09 +0200

    Make gallery display images and able upload of new ones

--- app/gallery/Gallery.java
+++ app/gallery/Gallery.java
@@ -1,26 +1,56 @@
 package gallery;
 
+import exceptions.NoFileInRequest;
+import exceptions.ServerError;
+import exceptions.Unauthorized;
+import models.GalleryImage;
+import models.Event;
+import models.HttpRequestData;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.layout;
+import controllers.Upload;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tormod on 06.10.2015.
  */
 public class Gallery extends Controller {
     public static Result display() {
-        ArrayList<String> images = new ArrayList<>();
-        images.add("a");
-        return ok(layout.render("Galleri", gallery.views.html.gallery.render(images)));
+        List<GalleryImage> images = GalleryImage.find.all();
+        List<String> urls = new ArrayList<>();
+        for (GalleryImage image : images) {
+            urls.add(image.getImageURL());
+        }
+        return ok(layout.render("Galleri", gallery.views.html.index.render(urls)));
     }
 
-    public static boolean upload() {
-        return false;
-    }
+    public static Result uploadGalleryImage() {
+        HttpRequestData data = new HttpRequestData();
+        String description = data.get("description");
+        String title = data.get("title");
+        Long eventId = data.getLong("eventId");
 
-    public static boolean upload(String arr) {
-        return false;
+        try {
+            String url = null;
+            System.out.println("adsdas");
+            url = Upload.upload();
+
+            if (url != null) {
+                Event event = null;
+                if (eventId != null) event = Event.find.byId(eventId);
+                GalleryImage image = new GalleryImage(title, description, event, url);
+                image.save();
+                return redirect(gallery.routes.Gallery.display());
+            } else throw new ServerError();
+        } catch (Unauthorized unauthorized) {
+            return unauthorized();
+        } catch (ServerError serverError) {
+            return internalServerError();
+        } catch (NoFileInRequest noFileInRequest) {
+            return badRequest();
+        }
     }
 }
