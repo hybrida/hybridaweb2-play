fd2b57e5955b4371fd7da8880f95262842918556
BourgondAries <macocio@gmail.com>
2015-10-20 19:19:02 +0200

    Prettify some code

--- app/bedkomdatabase/models/Contact.java
+++ app/bedkomdatabase/models/Contact.java
@@ -17,75 +17,75 @@ import models.*;
 
 @Entity
 public class Contact extends Model {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(nullable = false)
-    long contactId;
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(nullable = false)
+	long contactId;
 
-    @CreatedTimestamp
-    private Timestamp dateMade;
+	@CreatedTimestamp
+	private Timestamp dateMade;
 
-    @UpdatedTimestamp
-    private Timestamp whenUpdated;
+	@UpdatedTimestamp
+	private Timestamp whenUpdated;
 
-    private String contact;
-    private String telephone;
-    private String email;
-    @Column(columnDefinition = "text")
-    private String other;
+	private String contact;
+	private String telephone;
+	private String email;
+	@Column(columnDefinition = "text")
+	private String other;
 
-    private User madeBy;
+	private User madeBy;
 
-    @ManyToOne()
-    @JsonBackReference
-    private Bedrift bedrift;
+	@ManyToOne()
+	@JsonBackReference
+	private Bedrift bedrift;
 
-    public Contact(String telephone, String contact, String other, String email, Bedrift bedrift) {
-        this.telephone = telephone;
-        this.contact = contact;
-        this.other = other;
-        this.email = email;
-        this.bedrift = bedrift;
-        this.madeBy = LoginState.getUser();
-    }
+	public Contact(String telephone, String contact, String other, String email, Bedrift bedrift) {
+		this.telephone = telephone;
+		this.contact = contact;
+		this.other = other;
+		this.email = email;
+		this.bedrift = bedrift;
+		this.madeBy = LoginState.getUser();
+	}
 
-    public String getContact() {
-        return contact;
-    }
+	public String getContact() {
+		return contact;
+	}
 
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+	public void setContact(String contact) {
+			this.contact = contact;
+	}
 
-    public String getTelephone() {
-        return telephone;
-    }
+	public String getTelephone() {
+			return telephone;
+	}
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+	public void setTelephone(String telephone) {
+			this.telephone = telephone;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+			return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+			this.email = email;
+	}
 
-    public String getOther() {
-        return other;
-    }
+	public String getOther() {
+			return other;
+	}
 
-    public void setOther(String other) {
-        this.other = other;
-    }
+	public void setOther(String other) {
+			this.other = other;
+	}
 
-    public static Finder<Long, Contact> find = new Finder<>(
-            Long.class, Contact.class
-    );
+	public static Finder<Long, Contact> find = new Finder<>(
+					Long.class, Contact.class
+	);
 
-    public long getContactId() {
-        return contactId;
-    }
+	public long getContactId() {
+			return contactId;
+	}
 }
