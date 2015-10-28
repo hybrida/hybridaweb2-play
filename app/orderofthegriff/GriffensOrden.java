61ef3b07778fe64658603067f2862d2a5e928a2b
BourgondAries <macocio@gmail.com>
2015-10-28 19:47:25 +0100

    Rename griffens orden.

--- app/orderofthegriff/GriffensOrden.java
+++ app/orderofthegriff/GriffensOrden.java
@@ -8,7 +8,7 @@ import views.html.layoutWithHead;
 /**
  * Created by Torsten on 11.02.15
  */
-public class griffensOrden extends Controller {
+public class GriffensOrden extends Controller {
     final static public Result index() {  return ok(layoutWithHead.render("Griffens Orden", orderofthegriff.views.html.griffensOrdenbody.render(),orderofthegriff.views.html.griffensOrdenhead.render()));
     }
 }
