0e40b47feebf6458805e9d2034ac5958c5f12a24
BourgondAries <macocio@gmail.com>
2015-10-29 19:13:31 +0100

    Implement an authorization check and the bedkom control for prikking.

--- app/bedpressblock/BedPresBlock.java
+++ app/bedpressblock/BedPresBlock.java
@@ -12,20 +12,34 @@ import bedpressblock.views.html.*;
 import java.util.List;
 
 public class BedPresBlock extends Controller {
+
+	private static boolean checkAccess() {
+		return !LoginState.getUser().isInBedkom();
+	}
+
+	private static Result unauthorize() {
+		return redirect(application.routes.Application.showUnauthorizedAccess());
+	}
+
 	public static Result index() {
-		// Choose an event to inspect
+		if (checkAccess()) return unauthorize();
+
 		List<Event> bedpresses = Event.find.setMaxRows(10).where(
 			).eq("bedpres", true).findList();
 		return ok(pickbedpres.render(bedpresses));
 	}
 
 	public static Result pickUsers() {
+		if (checkAccess()) return unauthorize();
+
 		HttpRequestData post = new HttpRequestData();
 		Long eventId = post.getLong("eventId");
 		return pickUsers(eventId);
 	}
 
 	public static Result pickUsers(Long eventId) {
+		if (checkAccess()) return unauthorize();
+
 		if (eventId == 0)
 			return redirect(bedpressblock.routes.BedPresBlock.index().url());
 		Event event = Event.find.byId(eventId);
@@ -33,6 +47,8 @@ public class BedPresBlock extends Controller {
 	}
 
 	public static Result blockUser() {
+		if (checkAccess()) return unauthorize();
+
 		HttpRequestData post = new HttpRequestData();
 		Long eventId = post.getLong("eventId"),
 			userId = post.getLong("userId");
