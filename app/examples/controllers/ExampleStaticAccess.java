400748c8c11d245ca7b3b81153eb6ea190ab11a4
Ivar H. Krabol <ivarhk@gmail.com>
2015-10-30 05:32:01 +0100

    Added limited access example to routes

--- app/examples/controllers/ExampleStaticAccess.java
+++ app/examples/controllers/ExampleStaticAccess.java
@@ -4,7 +4,7 @@ import models.LoginState;
 import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.layout;
+import views.html.layoutBoxPage;
 
 /**
  * Created by ivar on 28.10.2015.
@@ -14,8 +14,9 @@ public class ExampleStaticAccess extends Controller {
     static public Result index() {
         if(!LoginState.isValidlyLoggedIn()) return redirect(sso.routes.SSOLogin.login(request().path()));
         if(!LoginState.getUser().loggedInUserHasAccess(true, User.Access.ADMIN)) return application.Application.showUnauthorizedAccess();
-        return ok(layout.render(
+        return ok(layoutBoxPage.render(
                 "Example Access",
-                examples.views.html.exampleStaticAccess.render()));
+                examples.views.html.exampleStaticAccess.render(),
+                null));
     }
 }
