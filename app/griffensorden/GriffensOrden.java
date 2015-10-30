cfa151f2e83bf45c2f666488c89746c5aa429b15
Ivar H. Krabol <ivarhk@gmail.com>
2015-10-30 03:58:35 +0100

    Clean up griffensorden and organize layoutinternals head. For real this time.

--- app/griffensorden/GriffensOrden.java
+++ app/griffensorden/GriffensOrden.java
@@ -9,6 +9,49 @@ import views.html.layoutWithHead;
  * Created by Torsten on 11.02.15
  */
 public class GriffensOrden extends Controller {
-    final static public Result index() {  return ok(layoutWithHead.render("Griffens Orden", griffensorden.views.html.griffensOrdenbody.render(), griffensorden.views.html.griffensOrdenhead.render()));
+    static public Result index() {  return ok(layoutWithHead.render("Griffens Orden", griffensorden.views.html.griffensOrdenBody.render(), griffensorden.views.html.griffensOrdenHead.render()));
     }
+
+    static public Result riddere() {
+        return ok(
+                layoutWithHead.render(
+                        "Griffens Orden",
+                        griffensorden.views.html.riddereBody.render(),
+                        griffensorden.views.html.griffensOrdenHead.render()
+                )
+        );
+    }
+    static public Result nomination() {  return ok(layoutWithHead.render(
+            "Griffens Orden Nominasjon",
+            griffensorden.views.html.nominationBody.render(),
+            griffensorden.views.html.griffensOrdenHead.render()));
+
+    }
+/**
+ public static Result save() {
+ try {
+ User user = LoginState.getUser();
+ if (user == null || !user.canCreateNewArticle())
+ return application.Application.showUnauthorizedAccess();
+ models.Article art = saveArticle();
+ if (!(new HttpRequestData().get("event") == null)) {
+ ResultAndEId res = saveEvent(art, null);
+ if (res.result != null)
+ return res.result;
+ Renders.addEvent(res.event);
+ return redirect(article.routes.Event.viewEvent("" + res.event.getId()));
+ }
+ else {
+ Renders.addArticle(art);
+ return redirect(article.routes.Article.viewArticle("" + art.getId()));
+ }
+ }
+ catch (IllegalStateException e) {
+ return application.Application.show400("ugyldig data oppgitt: " + e);
+ }
+ catch (Exception e) {
+ e.printStackTrace();
+ return ok(e.toString());
+ }
+ } **/
 }
