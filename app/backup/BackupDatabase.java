a76ef3dfebb0565a7cdb85da50fd1e9c47ee5818
Ivar Krabol <ivarhk@gmail.com>
2015-11-05 12:46:55 +0100

    Adjust backup to work with mysql database

--- app/backup/BackupDatabase.java
+++ app/backup/BackupDatabase.java
@@ -10,9 +10,11 @@ import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Tormod on 04.11.2014.
@@ -30,48 +32,41 @@ public class BackupDatabase {
 		return Results.internalServerError("Couldn't backup database!");
 	}
 
-	private static ArrayList<Reader> h2Readers(String user, String password) throws SQLException {
+	private static Stream<String> h2ExpressionStream(String user, String password) throws SQLException {
 		javax.sql.DataSource ds = DB.getDataSource();
 		java.sql.Connection connection = ds.getConnection(user, password);
 		java.sql.Statement statement = connection.createStatement();
 		ResultSet result = statement.executeQuery("SCRIPT SIMPLE NOSETTINGS"); // TO 'conf\\evolutions\\default\\2.sql'");
-		ArrayList<Reader> readers = new ArrayList<>();
-		while (result.next()) readers.add(new InputStreamReader(result.getAsciiStream(1)));
+		ArrayList<String> expressions = new ArrayList<>();
+		while (result.next()) expressions.add(result.getString(1));
 		result.close();
-		return readers;
+		return expressions.stream();
 	}
 
-	private static ArrayList<Reader> mysqlReaders(String user, String password) throws IOException {
+	private static Stream<String> mysqlExpressionStream(String user, String password) throws Exception {
 		Runtime rt = Runtime.getRuntime();
-		String[] commands = {"mysqldump","-u "+user+" -p"+password+" hybrida"};
+		String[] commands = {"mysqldump","-h127.0.0.1", "-u"+user, "-p"+password, "hybrida"};
 		Process proc = rt.exec(commands);
+
+		BufferedReader stdError = new BufferedReader(
+				new InputStreamReader(proc.getErrorStream()));
+		if ((stdError.ready())) throw new Exception(stdError.readLine());
 
 		BufferedReader stdInput = new BufferedReader(new
 				InputStreamReader(proc.getInputStream()));
-
-		BufferedReader stdError = new BufferedReader(new
-				InputStreamReader(proc.getErrorStream()));
-
-// read the output from the command
-		System.out.println("Here is the standard output of the command:\n");
-		String s = null;
-		while ((s = stdInput.readLine()) != null) {
-			System.out.println(s);
-		}
-
-// read any errors from the attempted command
-		System.out.println("Here is the standard error of the command (if any):\n");
-		while ((s = stdError.readLine()) != null) {
-			System.out.println(s);
-		}
-		return null;
+		return stdInput.lines();
 	}
 
 	private static Boolean backup() {
-		ArrayList<Reader> readers = new ArrayList<>();
-		String ups = "";
-		String downs = "";
-		ArrayList<String> table_names = new ArrayList<>();
+		Stream<String> expressions = null;
+		StringBuilder ups = new StringBuilder("# --- !Ups");
+		ups.append(System.lineSeparator());
+		int upsTopSize = ups.length();
+		StringBuilder downs = new StringBuilder("# --- !Downs");
+		downs.append(System.lineSeparator());
+		downs.append("SET FOREIGN_KEY_CHEKCS = 0;");
+		downs.append(System.lineSeparator());
+		ArrayList<String> tables = new ArrayList<>();
 		String everything = "";
 		List<String> lines = new ArrayList<>();
 		try {
@@ -80,16 +75,18 @@ public class BackupDatabase {
 			String dbPassword = conf.getString("db.default.password");
 			switch(conf.getString("db.default.name")){
 				case "mysql":
-					readers = mysqlReaders(dbUser, dbPassword);
+					expressions = mysqlExpressionStream(dbUser, dbPassword);
 					break;
 				default:
-					readers = h2Readers(dbUser, dbPassword);
+					expressions = h2ExpressionStream(dbUser, dbPassword);
 					break;
 			}
 			int count_of_qout = 0;
-			for(Reader reader : readers) {
-				while (reader.ready()) {
-					char character = (char) reader.read();
+			for(String expression : expressions.collect(Collectors.toList())) {
+				Reader reader = new StringReader(expression);
+				int read;
+				while (reader.ready() && (read = reader.read()) != -1) {
+					char character = (char) read;
 					everything += character;
 					if (character == '\'') count_of_qout++;
 					else if (character == ';' && count_of_qout%2 == 0) {
@@ -102,24 +99,30 @@ public class BackupDatabase {
 				}
 				reader.close();
 			}
+			Pattern insertPattern = Pattern.compile("^insert into `?([a-z0-9_]+)\\b", Pattern.CASE_INSENSITIVE);
 			for (String line : lines) {
-				String upper_line = line.toUpperCase();
-				if (upper_line.startsWith("INSERT") && !upper_line.startsWith("INSERT INTO SYSTEM_LOB_STREAM") && !upper_line.startsWith("INSERT INTO PUBLIC.PLAY_EVOLUTIONS")) {
-					if (upper_line.startsWith("INSERT INTO PUBLIC.USER")) ups = line + ups;
-					else ups += line;
-					String table_name = line.substring(12, upper_line.indexOf("(", 12));
-					if (!table_names.contains(table_name)) {
-						table_names.add(table_name);
-						downs += "TRUNCATE TABLE " + table_name + ";" + System.lineSeparator(); //18 is the location after INSERT INTO PUBLIC.
+				Matcher insert = insertPattern.matcher(line);
+				String table;
+				if (insert.find() && !(table = insert.group(1).toLowerCase()).matches("system_lob_stream|play_evolutions")) {
+					line = line.replaceAll(insert.group(0), "INSERT INTO `" + table);
+					line = line.replaceAll("VALUES ", "VALUES" + System.lineSeparator() + "\t");
+					line = line.replaceAll("\\),\\(", ")," + System.lineSeparator() + "\t(");
+					if (table.equals("user")) ups.insert(upsTopSize, line); // Make sure users are at the top
+					else ups.append(line);
+					if (!tables.contains(table)) {
+						tables.add(table);
+						downs.append("TRUNCATE TABLE `");
+						downs.append(table);
+						downs.append("`;");
+						downs.append(System.lineSeparator());
 					}
 				}
 			}
 
 
 			String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-			BufferedWriter writer = new BufferedWriter(new FileWriter("./conf/evolutions/default/backup/" + now + ".sql")); //When it's going to be used, call it 2.sql or something
-			ups = "# --- !Ups" + System.lineSeparator() + ups;
-			downs = "# --- !Downs" + System.lineSeparator() + "SET FOREIGN_KEY_CHECKS = 0;" + System.lineSeparator() + downs + "SET FOREIGN_KEY_CHECKS = 1;";
+			BufferedWriter writer = new BufferedWriter(new FileWriter("./conf/evolutions/default/backup/" + now + ".sql"));
+			downs.append("SET FOREIGN_KEY_CHECKS = 1;");
 			writer.write(ups + System.lineSeparator() + downs);
 			writer.flush();
 			writer.close();
