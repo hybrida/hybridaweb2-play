d4afe8527226eed6974a1496115f255133d89b37
simen <simennj@outlook.com>
2015-11-11 00:23:38 +0100

    rewrite update^k pdf listing

--- app/update/Update.java
+++ app/update/Update.java
@@ -7,6 +7,8 @@ import views.html.layout;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 import static play.mvc.Controller.flash;
@@ -18,20 +20,16 @@ import static play.mvc.Results.ok;
 public class Update {
 
     public static Result index() throws FileNotFoundException {
-
-
-        return ok(layout.render("Update!", update.views.html.index.render(getFiles())));
-    }
-    //"gs -sDEVICE=png16m -o public/update/" + thumbName + " public/update/" + fileName r243x313
-
-    public static HashMap<String, String[]> getFiles() {
-        String[] foldernamess = new File("public/update").list((dir, name) -> dir.isDirectory());
+        String[] foldernamess = new File("public/update").list((dir, name) -> !name.contains("."));
+        Arrays.sort(foldernamess, Comparator.<String>reverseOrder());
         HashMap<String, String[]> folders = new HashMap<String, String[]>();
         for (String foldername : foldernamess) {
             folders.put(foldername, new File("public/update/"+foldername).list((dir, name) -> name.toLowerCase().endsWith(".png")));
         }
-        return folders;
+
+        return ok(layout.render("Update!", update.views.html.index.render(foldernamess, folders)));
     }
+    //"gs -sDEVICE=png16m -o public/update/" + thumbName + " public/update/" + fileName r243x313
 
     public static Result utgaver() {
         return TODO;
