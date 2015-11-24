6bfc9af35743f3c6df9e2af09a5d9c44e47a6e3a
Andreas <anstra@stud.ntnu.no>
2015-11-24 19:22:06 +0100

    ITK ringen siden.

--- app/iktringen/IKTringen.java
+++ app/iktringen/IKTringen.java
@@ -1,15 +1,16 @@
 package iktringen;
 
+import play.mvc.Controller;
+import play.mvc.Result;
+
 /**
  * Created by Andreas on 13/10/2015.
  */
-public class IKTringen extends play.mvc.Controller {
-    public static play.mvc.Result index() {
-        return ok(
-                views.html.layout.render(
-                        "IKTringen",
-                        iktringen.views.html.index.render()
-                )
-        );
+public class IKTringen extends Controller {
+    final static public Result index() {
+        return ok(views.html.layoutWithHead.render(
+                "iktringen nettside",
+                iktringen.views.html.index.render(),
+                iktringen.views.html.head.render()));
     }
 }
