0f481aa25a716d4a414e90c106dcbf0e63d3cb70
BourgondAries <macocio@gmail.com>
2015-11-24 20:43:52 +0100

    Add the RFID card reader

---
+++ app/rfid/RFIDReader.java
@@ -0,0 +1,52 @@
+package rfid;
+
+import java.nio.ByteBuffer;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import rfid.views.html.*;
+import models.HttpRequestData;
+
+public class RFIDReader extends Controller {
+
+	public static Result index() {
+		return ok(reader.render());
+	}
+
+	public static Result read() {
+		HttpRequestData htpdata = new HttpRequestData();
+		System.out.println(
+			htpdata.get("rfid"));
+		Long rfidRead = htpdata.getLong("rfid");
+		System.out.println(invertBytes(rfidRead));
+		return redirect(rfid.routes.RFIDReader.index());
+	}
+
+	public static Long invertBytes(Long rfidIn) {
+		ByteBuffer byteBuffer = ByteBuffer.allocate(Long.BYTES);
+		byteBuffer.putLong(rfidIn);
+
+
+		// Try with: 004939781, or, 0004937373
+		final byte nil = 0;
+		final byte one = 1;
+		final byte[] currentByte = byteBuffer.array();
+		byte[] finalByte = new byte[Long.BYTES];
+
+		for (byte j = 0; j < Long.BYTES; ++j) {
+			for (byte i = 0; i < 8; ++i) {
+				final byte oneCopy = (byte) (one << i);
+				final byte newByte = (byte) ((currentByte[j] & oneCopy) >> i);
+				finalByte[j] = (byte) (finalByte[j] | (byte) (newByte << (7 - i)));
+			}
+		}
+
+		System.out.println(Integer.toBinaryString(currentByte[7] & 255 | 256).substring(1));
+		System.out.println(Integer.toBinaryString(finalByte[7] & 255 | 256).substring(1));
+
+		byteBuffer = ByteBuffer.wrap(finalByte);
+		return byteBuffer.getLong();
+	}
+
+}
