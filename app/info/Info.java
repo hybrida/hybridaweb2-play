dedcafc1d437cf382d9d457dba1fc587846de1fe
Ivar Krabol <ivarhk@stud.ntnu.no>
2016-02-02 22:28:38 +0100

    Change name of 'abouthybrida' to 'info'

--- app/info/Info.java
+++ app/info/Info.java
@@ -1,4 +1,4 @@
-package abouthybrida;
+package info;
 
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -6,11 +6,11 @@ import play.mvc.Result;
 /**
  * Created by Kia on 16.10.14.
  */
-public class AboutHybrida extends Controller {
+public class Info extends Controller {
 	final static public Result index() {
 		return ok(views.html.layoutWithHead.render(
 				"Hybrida - Om Hybrida",
-				abouthybrida.views.html.index.render(),
-				abouthybrida.views.html.head.render()));
+				info.views.html.index.render(),
+				info.views.html.head.render()));
 	}
 }
