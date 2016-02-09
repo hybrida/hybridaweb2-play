6563bf72c6b4171efb35c560f04b792ec838190f
Ivar Krabol <ivarhk@stud.ntnu.no>
2016-02-09 23:51:57 +0100

    Clean up bloated and incorrectly used code

--- app/info/Info.java
+++ app/info/Info.java
@@ -3,63 +3,75 @@ package info;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.layout;
+import views.html.layoutWithHead;
 
 /**
  * Created by Kia on 16.10.14.
  */
 public class Info extends Controller {
 	public static Result index() {
-		return ok(views.html.layoutWithHead.render(
-				"Om Hybrida",
-				info.views.html.index.render(),
-				info.views.html.head.render()));
+		return ok(
+				layoutWithHead.render(
+						"Om Hybrida",
+						info.views.html.index.render(),
+						info.views.html.head.render()
+				)
+		);
 	}
 
 	public static Result styret() {
-		return ok(layout.render(
-				"Styret",
-				info.views.html.styret.render()));
+		return ok(
+				layout.render(
+						"Styret",
+						info.views.html.styret.render()
+				)
+		);
 	}
 
 	public static Result arrkom() {
-		return ok(layout.render(
-				"Arrangementkomitéen", info.views.html.arrkom.render()));
+		return ok(
+				layoutWithHead.render(
+						"Arrangementkomitéen",
+						info.views.html.arrkom.render(),
+						info.views.html.staticpageHead.render()
+				)
+		);
 	}
 
 	public static play.mvc.Result vevkom() {
 		return ok(
-				views.html.layoutWithHead.render(
+				layoutWithHead.render(
 						"Vevkom",
 						info.views.html.vevkom.render(),
-						info.views.html.vevkomHead.render()
+						info.views.html.staticpageHead.render()
 				)
 		);
 	}
 
 	public static play.mvc.Result bedkom() {
 		return ok(
-				views.html.layoutWithHead.render(
+				layoutWithHead.render(
 						"Bedkom",
-						info.views.html.bedkomHead.render(),
-						info.views.html.bedkomBody.render()
+						info.views.html.bedkom.render(),
+						info.views.html.staticpageHead.render()
 				)
 		);
 	}
 
 	public static play.mvc.Result jentekom() {
 		return ok(
-				views.html.layoutWithHead.render(
+				layoutWithHead.render(
 						"Jentekom",
 						info.views.html.jentekom.render(),
-						info.views.html.jentekomHead.render()
+						info.views.html.staticpageHead.render()
 				)
 		);
 	}
 
 	public static Result redaksjonen() {
 		return ok(
-				views.html.layout.render(
-						"Hybrida - Redaksjonen",
+				layout.render(
+						"Redaksjonen",
 						info.views.html.redaksjonen.render()
 				)
 		);
