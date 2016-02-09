6564c326d039538953e8febe7c126efc847fa7eb
Ivar Krabol <ivarhk@stud.ntnu.no>
2016-02-09 21:16:19 +0100

    Move aboutstyret into info

--- app/info/Info.java
+++ app/info/Info.java
@@ -2,15 +2,21 @@ package info;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.layout;
 
 /**
  * Created by Kia on 16.10.14.
  */
 public class Info extends Controller {
-	final static public Result index() {
+	static public Result index() {
 		return ok(views.html.layoutWithHead.render(
 				"Hybrida - Om Hybrida",
 				info.views.html.index.render(),
 				info.views.html.head.render()));
+	}
+	static public Result styret() {
+		return ok(layout.render(
+				"Hybrida - Styret",
+				info.views.html.styret.render()));
 	}
 }
