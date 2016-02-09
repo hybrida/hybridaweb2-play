a85faf91acf1b1e6ef3fc5cb299fec59b37c6a21
Ivar Krabol <ivarhk@stud.ntnu.no>
2016-02-09 22:17:00 +0100

    Move bedkom, jentekom, redaksjonen & vevkom into info

--- app/info/Info.java
+++ app/info/Info.java
@@ -8,14 +8,14 @@ import views.html.layout;
  * Created by Kia on 16.10.14.
  */
 public class Info extends Controller {
-	static public Result index() {
+	public static Result index() {
 		return ok(views.html.layoutWithHead.render(
 				"Om Hybrida",
 				info.views.html.index.render(),
 				info.views.html.head.render()));
 	}
 
-	static public Result styret() {
+	public static Result styret() {
 		return ok(layout.render(
 				"Styret",
 				info.views.html.styret.render()));
@@ -24,5 +24,44 @@ public class Info extends Controller {
 	public static Result arrkom() {
 		return ok(layout.render(
 				"Arrangementkomitéen", info.views.html.arrkom.render()));
+	}
+
+	public static play.mvc.Result vevkom() {
+		return ok(
+				views.html.layoutWithHead.render(
+						"Vevkom",
+						info.views.html.vevkom.render(),
+						info.views.html.vevkomHead.render()
+				)
+		);
+	}
+
+	public static play.mvc.Result bedkom() {
+		return ok(
+				views.html.layoutWithHead.render(
+						"Bedkom",
+						info.views.html.bedkomHead.render(),
+						info.views.html.bedkomBody.render()
+				)
+		);
+	}
+
+	public static play.mvc.Result jentekom() {
+		return ok(
+				views.html.layoutWithHead.render(
+						"Jentekom",
+						info.views.html.jentekom.render(),
+						info.views.html.jentekomHead.render()
+				)
+		);
+	}
+
+	public static Result redaksjonen() {
+		return ok(
+				views.html.layout.render(
+						"Hybrida - Redaksjonen",
+						info.views.html.redaksjonen.render()
+				)
+		);
 	}
 }
