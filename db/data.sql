75a9fc23c34821d1d47d42c80939916771958a51
Bourgond Aries <macocio@gmail.com>
2016-04-05 20:03:36 +0200

    Add the other specializations

--- db/data.sql
+++ db/data.sql
@@ -1,1 +1,7 @@
 insert into specialization (description) values ('Geomatikk');
+insert into specialization (description) values ('Konstruksjonsteknikk');
+insert into specialization (description) values ('Marin teknikk');
+insert into specialization (description) values ('Produktutvikling og materialer');
+insert into specialization (description) values ('Petroleumsfag');
+insert into specialization (description) values ('Produksjonsledelse');
+insert into specialization (description) values ('Varme- og strømningsteknikk');
