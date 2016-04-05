5ad918e50c50492f8a9b00ece7624010f66a6278
Bourgond Aries <macocio@gmail.com>
2016-04-05 19:52:15 +0200

    Add events
    The value for 'graduationlimits' is a string of years.
    The years are relative to now, from 1 to 5. These classes
    can join. Ten of these are to be expected.
    
    "2345|12345"
    
    This means that the first sign up can allow any of the classes from 2 to 5, the second
    sign up from 1 to 5.

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
 create sequence user_seq;
-
 create table hybrid (
 	id bigint primary key not null default nextval('user_seq'),
 	username varchar(255) not null,
@@ -30,3 +29,36 @@ create table hybrid (
 	dateofbirth timestamp default null,
 	lastLogin timestamp default null
 );
+
+create sequence article_seq;
+create table article (
+	id bigint primary key not null default nextval('article_seq'),
+	title varchar(255),
+	ingress varchar(255),
+	text text,
+	datemade timestamp,
+	imagepath varchar(255)
+);
+alter table article add previousArticle bigint default null references article(id);
+
+create sequence event_seq;
+create table event (
+	id bigint primary key not null default nextval('event_seq'),
+	article_id bigint not null references article(id),
+
+	location varchar(255),
+	graduationlimits varchar(10),
+	bedpres boolean default false,
+	binding boolean default false,
+	genderAllowed char default 'A',
+	maxParticipants int default 50,
+	maxWaiting int default 50,
+
+	signUpStart timestamp,
+	secondSignUp timestamp,
+	signUpDeadline timestamp,
+	eventHappens timestamp,
+	eventStops timestamp
+);
+alter table event add "previousEvent" bigint default null references event(id);
+
