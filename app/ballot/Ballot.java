fdb68b768b9fc433d1416d71a896cd9259f6fbf7
simen <simennj@outlook.com>
2016-04-10 18:38:59 +0200

    improve ballot

--- app/ballot/Ballot.java
+++ app/ballot/Ballot.java
@@ -1,13 +1,23 @@
 package ballot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.List;
 
 public class Ballot {
+    @JsonProperty
     public final String title;
+    @JsonProperty
     public final List<Candidate> results;
 
     public Ballot(String title, List<Candidate> results) {
         this.title = title;
         this.results = results;
+    }
+
+    @JsonProperty
+    public int total() {
+        return results.stream().mapToInt(c->c.votes).sum();
     }
 }
