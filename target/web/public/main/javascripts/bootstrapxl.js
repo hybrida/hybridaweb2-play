86bf0169d908fb66419eac57d0b0974e9b13d5cc
Thormod Myrvang <thormodm@stud.ntnu.no>
2016-04-19 18:46:07 +0200

    Revert "removes old background on statutter siden"
    This reverts commit 9e85471c32e981765d95b359c5bd4dd8209b1036.

--- target/web/public/main/javascripts/bootstrapxl.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("[class*='col-xl-']").each(function(){
-        var elem = $(this);
-        var classes = elem.attr("class");
-        if(classes.search(/\bcol-(xs|sm|md|lg)-(1[0-2]|[1-9])\b/) == -1) elem.addClass("col-xs-12");
-    });
-});
